3f7d1861eff89cfcd2b45b6246c2743ccb9249f9
LaPlazesDemon <164425143+LaPlazesDemon@users.noreply.github.com>
2024-05-23 01:36:28 -0400

    Created recap.js

---
+++ commands/recap.js
@@ -0,0 +1,51 @@
+const commandName = "Recap";
+
+const {SlashCommandBuilder} = require('discord.js');
+const axios = require("axios");
+const config = require("../config.json");
+const sql = require("../bot").sql;
+
+var log = function(...data) {console.log(`[${commandName}]`, data);};
+
+module.exports = {
+    info: {name: commandName, disabled: false},
+    data: new SlashCommandBuilder()
+    .setName("recap")
+    .setDescription("Recaps the channel activity from today"),
+    async execute(interaction) {
+        
+        if (interaction.channel.id == config.channels.adviceandrants) {
+            interaction.reply("I cannot ethically run this command within this channel");
+            return;
+        }
+
+        interaction.deferReply();
+        
+        sql.query(`SELECT * FROM overwatch.userMessages WHERE c_id = ? AND m_sent_at > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 1 DAY);`, [interaction.channel.id], function(err, result) {
+            if (err) {
+                interaction.editReply("There was an error getting the recap, apologies");      
+                log(err);
+            } else {
+                let messages = result;
+                let prompt = `${config.prompts.recap}\n\n`;
+                messages.forEach(m => prompt += `"${m['m_sender_name']}" said "${m['m_content']}" at ${m['m_sent_at']}\n`);
+            
+                axios.post("http://athena.home:11434/api/generate", { 
+                    model: "llama3:latest",
+                    prompt: prompt,
+                    stream: false
+                 })
+                .then(response => {
+                    interaction.editReply("Here is a 24 hour recap!\n\n"+response.data.response);
+                })
+                .catch(error => {
+                    interaction.editReply("There was an error getting the recap, apologies");     
+                    log(error);
+                });
+            }
+        })
+
+        
+
+    }
+} 
