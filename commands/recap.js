e2dd37557f35ec31b13231e3a2b2df1cae7de649
LaPlazesDemon <164425143+LaPlazesDemon@users.noreply.github.com>
2024-05-23 01:59:49 -0400

    Added splitting messages by paragraph to get around the 2000 character limit on discord messsages
    Also modified the SQL Query to avoid Astra feeding recaps to herself

--- commands/recap.js
+++ commands/recap.js
@@ -21,10 +21,12 @@ module.exports = {
 
         interaction.deferReply();
         
-        sql.query(`SELECT * FROM overwatch.userMessages WHERE c_id = ? AND m_sent_at > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 1 DAY);`, [interaction.channel.id], function(err, result) {
+        sql.query(`SELECT * FROM overwatch.userMessages WHERE c_id = ? AND m_sent_at > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 1 DAY) AND m_sender IS NOT 1113307512297492520;`, [interaction.channel.id], function(err, result) {
             if (err) {
                 interaction.editReply("There was an error getting the recap, apologies");      
                 log(err);
+            } else if (!result.length) {
+                interaction.editReply("There were no messages found to recap in the last 24 hours");
             } else {
                 let messages = result;
                 let prompt = `${config.prompts.recap}\n\n`;
@@ -36,7 +38,11 @@ module.exports = {
                     stream: false
                  })
                 .then(response => {
-                    interaction.editReply("*:warning:Please note that this command uses locally hosted LLMs and can make mistakes. It will be tweaked and refined as time passes but do not take what it says as gospel, more just as a catchup tool. Enjoy!*\nHere is a 24 hour recap!\n\n"+response.data.response);
+                    interaction.editReply("*:warning:Please note that this command uses locally hosted LLMs and can make mistakes. It will be tweaked and refined as time passes but do not take what it says as gospel, more just as a catchup tool. Enjoy!*\nHere is a 24 hour recap!");
+                    let paragraphs = response.data.response.split("\n");
+                    paragraphs.forEach(async paragraph => 
+                        await interaction.channel.send(paragraph)
+                    );
                 })
                 .catch(error => {
                     interaction.editReply("There was an error getting the recap, apologies");     
